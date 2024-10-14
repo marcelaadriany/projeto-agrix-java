e6b3543a6610824d411d900ba497b5d72c12fb37
marcelaadriany <marcela.adriany@gmail.com>
2024-10-14 18:39:09 -0300

    rota post /persons

---
+++ src/main/java/com/betrybe/agrix/controller/dto/PersonCreationDto.java
@@ -0,0 +1,19 @@
+package com.betrybe.agrix.controller.dto;
+
+import com.betrybe.agrix.entity.Person;
+import com.betrybe.agrix.security.Role;
+
+/**
+ * The type Person creation dto.
+ */
+public record PersonCreationDto(String username, String password, Role role) {
+
+  /**
+   * To entity person.
+   *
+   * @return the person
+   */
+  public Person toEntity() {
+    return new Person(username, password, role);
+  }
+}
---
+++ src/main/java/com/betrybe/agrix/entity/Person.java
@@ -0,0 +1,149 @@
+package com.betrybe.agrix.entity;
+
+
+import com.betrybe.agrix.security.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Class representing a person.
+ */
+@Entity
+public class Person implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
+  private String username;
+
+  @JsonIgnore
+  private String password;
+
+  private Role role;
+
+  /**
+   * Instantiates a new Person.
+   *
+   * @param username the username
+   * @param password the password
+   * @param role     the role
+   */
+  public Person(String username, String password, Role role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public Long getId() {
+    return id;
+  }
+
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+
+  /**
+   * Sets username.
+   *
+   * @param username the username
+   */
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  /**
+   * Sets password.
+   *
+   * @param password the password
+   */
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  /**
+   * Gets role.
+   *
+   * @return the role
+   */
+  public Role getRole() {
+    return role;
+  }
+
+  /**
+   * Sets role.
+   *
+   * @param role the role
+   */
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Person person = (Person) o;
+    return Objects.equals(id, person.id) && Objects.equals(username,
+        person.username) && Objects.equals(password, person.password)
+        && Objects.equals(role, person.role);
+  }
+}
+
